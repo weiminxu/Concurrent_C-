21fccca34458c9cc3beeaa5d2ae0ea7af90d4211
weiminxu <weiminxu1982@gmail.com>
2019-11-01 22:59:29 +1300

    common program(single thread)
    common program(single thread)

---
+++ Project1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void function_1()
+{
+	std::cout << "Beauty is only skin-deep" << std::endl;
+}
+
+int main()
+{
+	function_1();
+
+	return 0;
+}
